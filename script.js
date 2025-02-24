3bf564b561e6c864183347a8091bc32de83c6866
dakshatapatil11 <dakshatap33@gmail.com>
2025-02-24 17:55:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+           
+            
+        }
+    });
+});
+
+//document.getElementById('linkedin').addEventListener('click', function() {
+//     console.log('LinkedIn icon clicked');
+// });
+
+// document.getElementById('email').addEventListener('click', function() {
+//     console.log('Email icon clicked');
+// });
+
+// document.getElementById('twitter').addEventListener('click', function() {
+//     console.log('Twitter icon clicked');
+// });
+
+
+// script.js
+document.getElementById('resumeButton').addEventListener('click', function() {
+    var modal = document.getElementById('resumeModal');
+    modal.style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    var modal = document.getElementById('resumeModal');
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    var modal = document.getElementById('resumeModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
